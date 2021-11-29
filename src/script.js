efe07dc7d4c5ef5bbae573b2c5c426870182612b
DmitriyKalashnikov <verstkaone@gmail.com>
2021-11-29 19:33:04 +0200

    add

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-console.log("Как интересно!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -6,4 +8,57 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
+	devServer: {
+		port: 4200,
+		contentBase: path.join(__dirname, 'dist'),
+		watchContentBase: true,
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				include: /node_modules/,
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+					},
+				],
+			},
+			{
+				exclude: /node_modules/,
+				test: /\.(sa|sc)ss$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				exclude: /node_modules/,
+				test: /\.(js|jsx|mjs)$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							babelrc: true,
+						},
+					},
+				],
+			},
+		],
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	],
 };
